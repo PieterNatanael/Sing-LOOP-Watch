563a00b0e4c2e4d4010f3a637cb4c302488bc595
PieterNatanael <pieternatanael@icloud.com>
2024-04-08 21:45:06 +0800

    Initial Commit

---
+++ Sing LOOP Watch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sing LOOP Watch Watch App
+//
+//  Created by Pieter Yoshua Natanael on 08/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sing LOOP Watch Watch App/Sing_LOOP_WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sing_LOOP_WatchApp.swift
+//  Sing LOOP Watch Watch App
+//
+//  Created by Pieter Yoshua Natanael on 08/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Sing_LOOP_Watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
